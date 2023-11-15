3767d9e21431802a2265b0bd550def5e5cacae2d
Rafael <francorfs@icloud.com>
2023-11-15 09:16:01 -0300

    Criado arquivo JS

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+// let paragrafo = document.querySelector('#par1')
+
+// paragrafo.addEventListener('mouseover', trocarTexto)
+
+// function trocarTexto() {
+//     paragrafo.style.background='green'
+// }
